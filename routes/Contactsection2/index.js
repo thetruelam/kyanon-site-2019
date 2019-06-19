3e9434e9efbef98072f24073455e16f4d8d493e3
ThanhLy <thanh.ngly@kyanon.digital>
2019-06-19 23:33:11 +0700

    Responsive section 2 - contact us page -- version mobile

--- routes/Contactsection2/index.js
+++ routes/Contactsection2/index.js
@@ -29,8 +29,9 @@ class Section2 extends Component {
     const arrayImage = this.state.data.F13.split(',')
     return (
       <div className={`row ${styles.section2}`}>
-        <div className={styles.wrap}>
-          <div className={styles.leftBlock}>
+
+        <div className={`${styles.leftBlock} col-12 col-sm-12 col-lg-6 col-xl-6`}>
+          <div className={`${styles.left__padding}`}>
             <h1>OUR OFFICE</h1>
             <div>
               <h2>{this.state.data.F1}</h2>
@@ -56,27 +57,27 @@ class Section2 extends Component {
               {this.state.data.F11}
             </a>
           </div>
-          <div className={styles.rightBlock}>
-            <div className={styles.frameimg}>
-              <img
-                src={arrayImage[2]}
-                //src={`${props.backendDomain + images[0].url}`}
-                className={styles.photo_1}
-                alt=""
-              />
-              <img
-                src={arrayImage[1]} //Right
-                //src={`${props.backendDomain + images[1].url}`}
-                className={styles.photo_2}
-                alt=""
-              />
-              <img
-                src={arrayImage[0]}// Left
-                //src={`${props.backendDomain + images[2].url}`}
-                className={styles.photo_3}
-                alt=""
-              />
-            </div>
+        </div>
+        <div className={`${styles.rightBlock} col-12 col-sm-12 col-lg-6 col-xl-6`}>
+          <div className={styles.frameimg}>
+            <img
+              src={arrayImage[2]}
+              //src={`${props.backendDomain + images[0].url}`}
+              className={styles.photo_1}
+              alt=""
+            />
+            <img
+              src={arrayImage[1]} //Right
+              //src={`${props.backendDomain + images[1].url}`}
+              className={styles.photo_2}
+              alt=""
+            />
+            <img
+              src={arrayImage[0]}// Left
+              //src={`${props.backendDomain + images[2].url}`}
+              className={styles.photo_3}
+              alt=""
+            />
           </div>
         </div>
       </div>
